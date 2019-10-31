820fe92ea0254c88e0f82e109243b79c891ff231
PEDRO AGUAYO <aguayomartinezpedro@gmail.com>
2019-10-30 20:25:26 -0600

    Maquetado de la parte superior opciones de usuario

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -48,12 +48,24 @@
                                 <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                             </li>
                         @else
+                            <li class="nav-item">
+                                <a class="nav-link" href="">Inicio</a>
+                            </li>
+                            <li class="nav-item">
+                                    <a class="nav-link" href="">Subir photo</a>
+                            </li>
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="">
+                                        Mi perfil
+                                    </a>
+                                    <a class="dropdown-item" href="">
+                                        Configuración
+                                    </a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
